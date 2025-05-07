3c713d0760ee09e6a1acb911896a3f858f79c418
xlanex6 <alexduval71@gmail.com>
2025-05-07 16:27:11 +0200

    feat: refactor state management to use localStorage for products and cart

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,16 +3,18 @@ import { onMounted, ref } from 'vue';
 import Loading from "../components/Loading.vue";
 import Card from "../components/Product/Card.vue";
 import Cart from "../components/Cart.vue";
+import { useLocalStorage } from '@vueuse/core';
 
 const loading = ref(false);
-const products = ref([]);
+const products = useLocalStorage('products', []);
 
 
 onMounted(async () => {
+  if (products.value.length > 0) return
   loading.value = true;
   try {
-    const response = await fetch('https://fakestoreapi.com/products');
-    products.value = await response.json();
+      const response = await fetch('https://fakestoreapi.com/products');
+      products.value = await response.json();
   } catch (error) {
     console.error('Error fetching products:', error);
   } finally {
--- src/state.ts
+++ src/state.ts
@@ -1,7 +1,8 @@
 import { ref } from 'vue';
 import { Product } from './types/products';
+import { useLocalStorage } from '@vueuse/core';
 
-const cart = ref<Product[]>([]);
+const cart = useLocalStorage('cart', [] as Product[]);
 
 const addToCart = (product: Product) => {
   cart.value.push(product);
